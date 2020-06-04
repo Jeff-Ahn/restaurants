baee03cd95ed37e2253058bfd2312507ba3ac851
Jeff-Ahn <ahu8867@gmail.com>
2020-06-04 16:29:26 +0900

    connected mysql by express

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
+const fs = require("fs");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/customers", (req, res) => {
-  const customers = [
-    { id: 1, firstName: "John", lastName: "Doe" },
-    { id: 2, firstName: "saeki", lastName: "sibal" },
-    { id: 3, firstName: "hyeonguk", lastName: "ahn" },
-  ];
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
 
-  res.json(customers);
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect();
+
+app.get("/api/restaurants", (req, res) => {
+  connection.query("SELECT * FROM restaurants", (err, rows, fields) => {
+    console.log(rows);
+    res.send(rows);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// app.get("/api/customers", (req, res) => {
+//   const customers = [
+//     { id: 1, firstName: "John", lastName: "Doe" },
+//     { id: 2, firstName: "saeki", lastName: "sibal" },
+//     { id: 3, firstName: "hyeonguk", lastName: "ahn" },
+//   ];
+
+//   res.json(customers);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server started on port ${port}`);
+// });
